763f4b9b29605c8c5faf8253614be8b10fc08d9a
anand981165 <39356793-anand981165@users.noreply.replit.com>
2025-02-05 02:45:29 +0000

    User checkpoint: Fix: Correctly render Home.html template in webview.

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template('Home.html')
+     return render_template('Home.html')
 
 if __name__ == '__main__':
-  print('Server starting on http://0.0.0.0:3000')
   app.run(host='0.0.0.0', debug=True)
