9b0f8947cca0a65538d709b960c1933eab3bec16
khus-dev <anand981165@gmail.com>
2025-02-05 02:22:23 +0000

    added the flask

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+  return 'Hello, World!'
 
 if __name__ == '__main__':
-    print('Server starting on http://0.0.0.0:3000')
-    app.run(host='0.0.0.0', port=3000)
+  print('Server starting on http://0.0.0.0:3000')
+  app.run(host='0.0.0.0', debug=True)
